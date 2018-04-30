6d714a8641ffcb013122b1f5c2d4e54335dbe53f
Mahendra Yadav <mahendra.k12@gmail.com>
2018-04-30 18:09:41 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,8 +3,9 @@ def roll_call_dwarves(collection) # code an argument here
   collection.each_with_index { | element, index | puts "#{index + 1} #{element}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(collection) # code an argument here
   # Your code here
+  
 end
 
 def long_planeteer_calls# code an argument here
