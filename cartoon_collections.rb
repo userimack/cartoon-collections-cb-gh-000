20008acb19f4b1fafa6ee128afe6c5ff7050972e
Mahendra Yadav <mahendra.k12@gmail.com>
2018-04-30 18:15:24 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ end
 
 def long_planeteer_calls(collection) # code an argument here
   # Your code here
-  
+  collection.any? { |i| i.length >4 }
 end
 
 def find_the_cheese# code an argument here
